49131c1d2362a53a39689d66ea539ccd95416fea
kmykoh97 <kmykoh97@gmail.com>
2021-10-02 09:00:47 -0700

    added few test cases. Fixed some edge cases

--- src/client/urlshortener/tinyurl/lib/parser.py
+++ src/client/urlshortener/tinyurl/lib/parser.py
@@ -4,6 +4,7 @@ from bs4 import BeautifulSoup
 
 
 def parser(external_sites_url):
+    # we need to pecify our own header because some websites eg. CoinGecko implements security checks
     headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
        'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
@@ -14,8 +15,14 @@ def parser(external_sites_url):
     req = Request(url=reg_url, headers=headers) 
     external_sites_html = urlopen(req).read()
     soup = BeautifulSoup(external_sites_html, "html.parser")
-    title = soup.title.string
     
+    # some website's title is embedded very deep resulting in bs4 not able to scrap them
+    # we need to do exception for them manually
+    try:
+        title = soup.title.string
+    except (IOError, AttributeError):
+        title = "this url's title cannot be read because it is using complex framework"
+        
     return title
     
 # print(parser('https://www.coingecko.com/en'))
--- src/client/urlshortener/tinyurl/tests.py
+++ src/client/urlshortener/tinyurl/tests.py
@@ -9,24 +9,67 @@ class CustomTestCase(TestCase):
     def setUp(self):
         testlongurl = "https://www.coingecko.com/en/coins/deez-nuts"
         Url.objects.create(shorturl="f1db86", originalurl=testlongurl)
+        testlongurl = "https://github.com/kmykoh97"
+        Url.objects.create(shorturl="exshorturl", originalurl=testlongurl)
         
     # to test if db workable
     def test_db(self):
         testobject = Url.objects.get(shorturl="f1db86")
         # self.assertEqual(getattr(testobject, "originalurl"), "https://www.coingecko.com/en/coins/deez-nuts")
         self.assertEqual(testobject.originalurl, "https://www.coingecko.com/en/coins/deez-nuts")
+        testobject = Url.objects.get(shorturl="exshorturl")
+        self.assertEqual(testobject.originalurl, "https://github.com/kmykoh97")
     
     # to test if core url shortener algorithm is correct
     def test_core_cal_tiny(self):
+        # basic case for long url
         originalurldata = "https://www.google.com/search?q=ibm&oq=ibm&aqs=chrome..69i57j35i39l2j69i60l5.552j0j7&sourceid=chrome&ie=UTF-8"
+        tinyurl = UrlHandler.get_tinyurl(originalurldata) # shorten url
+        reversegetoriginalurl = UrlHandler.get_originalurl(tinyurl) # revert to original url
+        self.assertEqual(reversegetoriginalurl, originalurldata) # checks if core function can return correct original url from shortened url
+        
+        # basic case for short url
+        originalurldata = "https://www.youtube.com/"
         tinyurl = UrlHandler.get_tinyurl(originalurldata)
         reversegetoriginalurl = UrlHandler.get_originalurl(tinyurl)
-        self.assertEqual(reversegetoriginalurl, originalurldata) # checks if core function can return correct original url from shortened url
+        self.assertEqual(reversegetoriginalurl, originalurldata)
+        
+        # dirty db case
+        originalurldata = "https://github.com/kmykoh97"
+        tinyurl = UrlHandler.get_tinyurl(originalurldata)
+        reversegetoriginalurl = UrlHandler.get_originalurl(tinyurl)
+        self.assertEqual(reversegetoriginalurl, originalurldata)
+        originalurldata = "https://www.coingecko.com/en/coins/deez-nuts"
+        tinyurl = UrlHandler.get_tinyurl(originalurldata)
+        reversegetoriginalurl = UrlHandler.get_originalurl(tinyurl)
+        self.assertEqual(reversegetoriginalurl, originalurldata) 
+        
+        # repeated db case(cache hit)
+        # let's try for 10 repetitions, result shouldn't change
+        # we check for cache hit details in log(logging need to be enabled in lib/tiny.py)
+        for x in range(10):
+            originalurldata = "https://www.coingecko.com/en"
+            tinyurl = UrlHandler.get_tinyurl(originalurldata)
+            reversegetoriginalurl = UrlHandler.get_originalurl(tinyurl)
+            self.assertEqual(reversegetoriginalurl, originalurldata)
+        
+        # giving random string
+        # it should be correct, but not able to redirect
+        originalurldata = "example string"
+        tinyurl = UrlHandler.get_tinyurl(originalurldata)
+        reversegetoriginalurl = UrlHandler.get_originalurl(tinyurl)
+        self.assertEqual(reversegetoriginalurl, originalurldata)
         
     # to test if website content scraper is correct
     def test_title_parser(self):
+        # website without header check
         originalurl = "https://www.google.com/"
         titletag = parser(originalurl)
         self.assertEqual(titletag, "Google") # checks if bs4 scraper is working
+        
+        # website with header check
+        originalurl = "https://www.coingecko.com/en"
+        titletag = parser(originalurl)
+        self.assertEqual(titletag, "CoinGecko: Cryptocurrency Prices and Market Capitalization")
 
     # note that we do not need to check location service because it is api provided by ipstack
